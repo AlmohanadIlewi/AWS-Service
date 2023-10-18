6a813bf5aff53e6a1ae0fe48f4bb6095a9c0c67e
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-10-18 03:21:08 +0200

    Terraform to build Nginx Docker Container running on Docker using S3 Bucket on AWS

--- Terraform-Docker/Terraform-Nginx-Docker/variable.tf
+++ Terraform-Docker/Terraform-Nginx-Docker/variable.tf
@@ -4,14 +4,14 @@ variable "external_port" {
     # Typ der Variable ist eine Zahl (number)
     type = number
 
-    # Standardwert ist 5000, falls kein Wert angegeben wird
-    default = 5000
+    # Standardwert ist 8080, falls kein Wert angegeben wird
+    default = 8080
 
-    # Validierung: Überprüfen, ob der Wert entweder 5000 oder 80 ist
+    # Validierung: Überprüfen, ob der Wert entweder 8080 oder 80 ist
     validation {
-        condition = can(regex("5000|80", var.external_port))
+        condition = can(regex("8080|80", var.external_port))
 
         # Fehlermeldung, die angezeigt wird, wenn die Validierung fehlschlägt
-        error_message = "port values can only be 5000 or 80."
+        error_message = "port values can only be 8080 or 80."
     }
 }
